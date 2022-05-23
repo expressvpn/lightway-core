7dc4f376754c5b443d81fc15c604636592a3b082
expressvpn-raihaan-m <98149193+expressvpn-raihaan-m@users.noreply.github.com>
2022-05-23 17:31:28 +0800

    ios: Build iOS binaries on CI

--- ios/Lightway/build-xcframework.sh
+++ ios/Lightway/build-xcframework.sh
@@ -15,16 +15,8 @@ xcodebuild archive \
     -sdk iphonesimulator \
     SKIP_INSTALL=NO CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO PLATFORM=universal
 
-# macOS
-xcodebuild archive \
-    -scheme Lightway \
-    -archivePath "./build/macos.xcarchive" \
-    -sdk macosx \
-    SKIP_INSTALL=NO CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO PLATFORM=macos
-
 # Package XC Framework
 xcodebuild -create-xcframework \
     -framework "./Build/ios.xcarchive/Products/Library/Frameworks/Lightway.framework" \
     -framework "./Build/ios_sim.xcarchive/Products/Library/Frameworks/Lightway.framework" \
-    -framework "./Build/macos.xcarchive/Products/Library/Frameworks/Lightway.framework" \
     -output "./Build/Lightway.xcframework"
