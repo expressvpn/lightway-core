821273afc552d36405c6ee1a2f43211dc1876f6c
Tom L <tom.li@expressvpn.com>
2023-06-12 09:27:25 +0800

    tvos: add support of apple tvOS builds

--- ios/autotools-ios-helper.sh
+++ ios/autotools-ios-helper.sh
@@ -3,16 +3,22 @@
 set -e
 
 #
-# Build for iOS 64bit-ARM variants and iOS Simulator
-# - Place/copy the script at wolfSSL root
-# - Customize MIN_IOS_VERSION and other flags as needed
-#
-# Local Environment
+# Build for iOS device (arm64) and simulator (x86_64 only)
 # - macOS 10.15.4
 # - iOS 15.4
 # - Xcode 13.2.1
 #
+# Build for tvOS device (arm64) and simulator (x86_64 only)
+# - macOS 12.0
+# - tvOS 17.0
+# - Xcode 15.0 beta
+#
+# Usage:
+# - Place/copy the script at wolfSSL root
+# - Customize MIN_IOS_VERSION and other flags as needed
+#
 export MIN_IOS_VERSION=12.0
+export MIN_TVOS_VERSION=17.0
 
 build() {
     # Compiler options
@@ -33,6 +39,8 @@ build() {
     echo "Debug C_EXTRA_FLAGS=${WOLF_FLAGS}"
 
     export EXEC_PREFIX="${PREFIX}/${CONFIG}-${PLATFORM}"
+    echo "PLATFORM: ${PLATFORM}"
+    echo "EXEC_PREFIX: ${EXEC_PREFIX}"
     ./configure $ARCH_OPTS \
         C_EXTRA_FLAGS="$WOLF_FLAGS" \
         --host="${CHOST}" \
@@ -61,36 +69,66 @@ build() {
 }
 
 build_iphoneos() {
-    SDK="iphoneos"
-    PLATFORM="iphoneos"
-    ARCH_OPTS="--enable-armasm --enable-sp-asm"
-    ARCH_FLAGS="-arch arm64"
-    HOST_FLAGS="${ARCH_FLAGS} -miphoneos-version-min=${MIN_IOS_VERSION} -isysroot $(xcrun --sdk ${SDK} --show-sdk-path)"
-    CHOST="arm64-apple-ios"
+    export SDK="iphoneos"
+    export PLATFORM="iphoneos"
+    export EFFECTIVE_PLATFORM_NAME="-iphoneos"
+    export ARCH_OPTS="--enable-armasm --enable-sp-asm"
+    export ARCH_FLAGS="-arch arm64"
+    export HOST_FLAGS="${ARCH_FLAGS} -miphoneos-version-min=${MIN_IOS_VERSION} -isysroot $(xcrun --sdk ${SDK} --show-sdk-path)"
+    export CHOST="arm64-apple-ios"
     build
 }
 
 build_iphonesimulator() {
-    SDK="iphonesimulator"
-    PLATFORM="iphonesimulator"
-    ARCH_OPTS=""
-    ARCH_FLAGS="-arch x86_64"
-    HOST_FLAGS="${ARCH_FLAGS} -mios-simulator-version-min=${MIN_IOS_VERSION} -isysroot $(xcrun --sdk ${SDK} --show-sdk-path)"
-    CHOST="x86_64-apple-darwin"
+    export SDK="iphonesimulator"
+    export PLATFORM="iphonesimulator"
+    export EFFECTIVE_PLATFORM_NAME="-iphonesimulator"
+    export ARCH_OPTS=""
+    export ARCH_FLAGS="-arch x86_64"
+    export HOST_FLAGS="${ARCH_FLAGS} -mios-simulator-version-min=${MIN_IOS_VERSION} -isysroot $(xcrun --sdk ${SDK} --show-sdk-path)"
+    export CHOST="x86_64-apple-darwin"
     build
 }
 
-build_universal_binary() {
+build_tvos() {
+    export SDK="appletvos"
+    export PLATFORM="appletvos"
+    export EFFECTIVE_PLATFORM_NAME="-appletvos"
+    export ARCH_OPTS="--enable-armasm --enable-sp-asm"
+    export ARCH_FLAGS="-arch arm64"
+    export HOST_FLAGS="${ARCH_FLAGS} -mtvos-version-min=${MIN_TVOS_VERSION} -isysroot $(xcrun --sdk ${SDK} --show-sdk-path)"
+    export CHOST="arm64-apple-ios"
+    build
+}
+
+build_tvsimulator() {
+    export SDK="appletvsimulator"
+    export PLATFORM="appletvsimulator"
+    export EFFECTIVE_PLATFORM_NAME="-appletvsimulator"
+    export ARCH_OPTS=""
+    export ARCH_FLAGS="-arch x86_64"
+    export HOST_FLAGS="${ARCH_FLAGS} -mtvos-simulator-version-min=${MIN_TVOS_VERSION} -isysroot $(xcrun --sdk ${SDK} --show-sdk-path)"
+    export CHOST="x86_64-apple-darwin"
+    build
+}
+
+build_ios_universal_binary() {
     # Create ios universal binary
     LIB_NAME="libwolfssl.a"
-    mkdir -p "${PREFIX}/${CONFIG}-universal/lib"
-    lipo -create -output "${PREFIX}/${CONFIG}-universal/lib/${LIB_NAME}" \
+    mkdir -p "${PREFIX}/${CONFIG}-ios-universal/lib"
+    lipo -create -output "${PREFIX}/${CONFIG}-ios-universal/lib/${LIB_NAME}" \
         "${PREFIX}/${CONFIG}-iphoneos/lib/${LIB_NAME}" \
         "${PREFIX}/${CONFIG}-iphonesimulator/lib/${LIB_NAME}"
 }
 
-echo "Building wolfSSL for iOS..."
-
+build_tvos_universal_binary() {
+    # Create ios universal binary
+    LIB_NAME="libwolfssl.a"
+    mkdir -p "${PREFIX}/${CONFIG}-tvos-universal/lib"
+    lipo -create -output "${PREFIX}/${CONFIG}-tvos-universal/lib/${LIB_NAME}" \
+        "${PREFIX}/${CONFIG}-appletvos/lib/${LIB_NAME}" \
+        "${PREFIX}/${CONFIG}-appletvsimulator/lib/${LIB_NAME}"
+}
 # Locations
 PREFIX=${PREFIX:-"$(pwd)/../builds/wolfssl_ios"}
 CONFIG=${CONFIG:-"Release"}
@@ -100,19 +138,34 @@ UNIVERSAL="${PREFIX}"/universal
 mkdir -p "${PLATFORMS}"
 mkdir -p "${UNIVERSAL}"
 
-EFFECTIVE_PLATFORM_NAME=${1:-"universal"}
-case "${EFFECTIVE_PLATFORM_NAME}" in
+TARGET=${1:-"-all"}
+echo "Building wolfSSL for ${TARGET}..."
+
+case "${TARGET}" in
 -iphoneos)
     build_iphoneos
     ;;
 -iphonesimulator)
     build_iphonesimulator
     ;;
-*)
+-appletvos)
+    build_tvos
+    ;;
+-appletvsimulator)
+    build_tvsimulator
+    ;;
+-all)
     build_iphoneos
     build_iphonesimulator
-    build_universal_binary
+    build_ios_universal_binary
+    build_tvos
+    build_tvsimulator
+    build_tvos_universal_binary
+    ;;
+*)
+    echo "Unsupport target: ${TARGET}"
+    exit 64
     ;;
 esac
 
-echo "Building wolfSSL for iOS... Done."
+echo "Building wolfSSL for ${TARGET}... Done."
