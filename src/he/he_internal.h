6b811795e98d3bfadcef3b16d0fe52551ebf1c60
Mariappan Ramasamy <142216110+kp-mariappan-ramasamy@users.noreply.github.com>
2024-11-27 12:09:06 +0800

    Use minimal value

--- src/he/he_internal.h
+++ src/he/he_internal.h
@@ -54,18 +54,8 @@
 #ifndef thread_local
 # if __STDC_VERSION__ >= 201112 && !defined __STDC_NO_THREADS__
 #  define thread_local _Thread_local
-# elif defined _WIN32 && ( \
-       defined _MSC_VER || \
-       defined __ICL || \
-       defined __DMC__ || \
-       defined __BORLANDC__ )
+# elif defined _WIN32
 #  define thread_local __declspec(thread)
-/* note that ICC (linux) and Clang are covered by __GNUC__ */
-# elif defined __GNUC__ || \
-       defined __SUNPRO_C || \
-       defined __hpux || \
-       defined __xlC__
-#  define thread_local __thread
 # else
 #  error "Cannot define thread_local"
 # endif
